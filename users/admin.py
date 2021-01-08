ab6564aefcf0a785ee4d1c65ac3a9d85d61df54e
Ctdlog <73215539+Choi-HW980402@users.noreply.github.com>
2021-01-08 14:32:51 +0900

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.User)
+class CustomUserAdmin(admin.ModelAdmin):
+
+    """ Custom User Admin """
+
+    list_display = ("username", "email", "gender", "language", "currency", "superhost")
+    list_filter = (
+        "language",
+        "currency",
+        "superhost",
+    )
