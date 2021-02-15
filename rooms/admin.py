da165ca8853136e3a77eb762c49d4f495734ad58
Ctdlog <73215539+Choi-HW980402@users.noreply.github.com>
2021-02-15 23:01:11 +0900

    #6.0 Room Admin Panel, #6.1 Room Admin Panel part Two, #6.2 Custom Admin Functions

--- rooms/admin.py
+++ rooms/admin.py
@@ -17,7 +17,73 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        (
+            "Spaces",
+            {
+                "fields": (
+                    "guests",
+                    "beds",
+                    "bedrooms",
+                    "baths",
+                )
+            },
+        ),
+        (
+            (
+                "More About the Space",
+                {
+                    "classes": ("collapse",),
+                    "fields": ("amenities", "facilities", "house_rules"),
+                },
+            )
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    ordering = ("name", "price", "bedrooms")
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "city",
+        "amenities",
+        "facilities",
+        "country",
+        "house_rules",
+    )
+
+    search_fields = ("=city", "^host__username")
+
+    filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    def count_amenities(self, obj):
+        print(obj.amenities.all())
+        return "Potato"
+
+    count_amenities.short_description = "hello sexy!"
 
 
 @admin.register(models.Photo)
