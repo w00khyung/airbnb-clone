674b13a65301217a03f458eab6a2d11d6986d45a
Ctdlog <73215539+Choi-HW980402@users.noreply.github.com>
2021-02-23 23:05:37 +0900

    #7.3 Finising the Room Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -9,7 +9,10 @@ class ItemAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
 
-    pass
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
 
 
 @admin.register(models.Room)
@@ -60,6 +63,7 @@ class RoomAdmin(admin.ModelAdmin):
         "check_out",
         "instant_book",
         "count_amenities",
+        "count_photos",
     )
 
     ordering = ("name", "price", "bedrooms")
@@ -81,6 +85,9 @@ class RoomAdmin(admin.ModelAdmin):
 
     def count_amenities(self, obj):
         return obj.amenities.count()
+
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
