f7447c74bbb49ee8933658793978a2e6c0ec8a03
Ctdlog <73215539+Choi-HW980402@users.noreply.github.com>
2021-02-24 22:56:39 +0900

    #8.8 Intercepting Model save() and admin_save()

--- rooms/admin.py
+++ rooms/admin.py
@@ -31,7 +31,7 @@ class RoomAdmin(admin.ModelAdmin):
     fieldsets = (
         (
             "Basic Info",
-            {"fields": ("name", "description", "country", "address", "price")},
+            {"fields": ("name", "description", "country", "city", "address", "price")},
         ),
         ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
         (
@@ -49,7 +49,7 @@ class RoomAdmin(admin.ModelAdmin):
             (
                 "More About the Space",
                 {
-                    "classes": ("collapse",),
+                    # "classes": ("collapse",),
                     "fields": ("amenities", "facilities", "house_rules"),
                 },
             )
