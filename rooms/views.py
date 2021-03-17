386c889817ab379b07ceb354d430b41164f755f1
Ctdlog <73215539+Choi-HW980402@users.noreply.github.com>
2021-03-17 22:12:22 +0900

    #11.6 Handling Exceptions

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
 from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import redirect, render
+from django.core.paginator import EmptyPage, Paginator
 from . import models
 
 
@@ -11,5 +11,8 @@ def all_rooms(request):
     page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10, orphans=5)
-    rooms = paginator.get_page(int(page))
-    return render(request, "rooms/home.html", {"page": rooms})
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms})
+    except EmptyPage:
+        return redirect("/")
